064f768a28936c8cf1b6c0642bba4acddf2ff5e4
Ikenna Ibezim <45284274+iamikenna@users.noreply.github.com>
2021-02-23 15:13:01 -0500

    Login modal and Updates on home page

--- client/src/components/Header/NavBar.js
+++ client/src/components/Header/NavBar.js
@@ -14,11 +14,8 @@ const NavBar = () => {
             <div className = "nav-items">
                 <Link className = "nav-link" to='/Home'>Home</Link>
                 <Link className = "nav-link" to='/Register'>Extra Page</Link>
-                <a className = "nav-link" target='_blank' rel="noopener noreferrer" href="https://reactjs.org/docs/getting-started.html">
-                    React Docs
-                </a>
-                <a className = "nav-link" target="_blank" rel="noopener noreferrer" href="https://reactjs.org/tutorial/tutorial.html">React Tutorial</a>
-                <a className = "nav-link" target="_blank" rel="noopener norefferer" href="https://nodejs.org/en/docs/">Node Docs</a>
+                <Link className = "nav-link" to='/Signup'>Sign Up</Link>
+                
             </div>
 
         </div>
