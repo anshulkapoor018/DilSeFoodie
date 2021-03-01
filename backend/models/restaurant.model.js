387e2bc01d9f5f668ba022f237e551d45135ce26
Anshul Kapoor <django@Anshuls-MBP.fios-router.home>
2021-03-01 16:59:30 -0500

    seed Restaurants init

---
+++ backend/models/restaurant.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+// name, website, category, address, city, state, zip, latitude, longitude
+const restaurantSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  website: { type: String, required: true },
+  category: { type: String, required: true },
+  address: { type: String, required: true },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  zip: { type: String, required: true },
+  latitude: { type: Number, required: true },
+  longitude: { type: Number, required: true }
+});
+
+const restaurant = mongoose.model("Restaurant", restaurantSchema);
+module.exports = restaurant;
