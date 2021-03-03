03eed257532f0a244bf38cf7322fa5311e3e7ab6
NAruneshwar <57510031+NAruneshwar@users.noreply.github.com>
2021-03-02 19:20:51 -0500

    Models work API in progress

---
+++ backend/models/order.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+// name, website, category, address, city, state, zip, latitude, longitude
+const reviewSchema = new mongoose.Schema({
+  orderId: { type: String, required: true },
+  restaurantId: { type: String, required: true },
+  userId: { type: String, required: true },
+  payment: { type: String, required: true },
+  typeOfOrder: { type: String, required: true },
+  timeOfOrder: { type: String, required: true },
+  orderStatus: { type: String, required: true }
+}, {
+    timestamps: true,
+  });
+
+const review = mongoose.model("Order", reviewSchema);
+module.exports = review;
