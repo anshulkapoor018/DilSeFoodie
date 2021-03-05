43e65f45fc3a270e8a96be74c53c114c3147d84b
Ikenna Ibezim <45284274+iamikenna@users.noreply.github.com>
2021-03-05 15:00:03 -0500

    Refactored Navbar

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -3,44 +3,33 @@ import { Link } from 'react-router-dom';
 import './navbar.component.css';
 
 const NavBar = () => {
+    let path = '/user';
+    let name = 'Signup';
 
-    // const 
     if (window.sessionStorage.getItem('isLoggedIn') === null || window.sessionStorage.getItem('isLoggedIn') === 'false'){
-        return (
-            <div className = "header">
-                {/* Logo */}
-                <Link className = "nav-title" to="/">
-                    <img className = "nav-logo" src={ "/logo.png" } alt="React logo" />
-                </Link>
-
-                {/* Page Links */}
-                <div className = "nav-items">
-                    <Link className = "nav-link" to='/Home'>Home</Link>
-                    <Link className = "nav-link" to='/Restaurant'>Restaurant</Link>
-                    <Link className = "nav-link" to='/user'>Signup</Link>
-                </div>
-            </div>
-        )
-    } else {
-        var userObject = JSON.parse(window.sessionStorage.getItem("userDetails"));
-        return (
-            <div className = "header">
-                {/* Logo */}
-                <Link className = "nav-title" to="/">
-                    <img className = "nav-logo" src={ "/logo.png" } alt="React logo" />
-                </Link>
-    
-                {/* Page Links */}
-                <div className = "nav-items">
-                    <Link className = "nav-link" to='/Home'>Home</Link>
-                    <Link className = "nav-link" to='/Restaurant'>Restaurant</Link>
-                    <Link className = "nav-link" to='/profile'>{userObject['firstName']}</Link>
-                </div>
-    
-            </div>
-        )
+        path = path;
+        name = name;
     }
+    else{
+        var userObject = JSON.parse(window.sessionStorage.getItem("userDetails"));
+        path = '/profile';
+        name = userObject['firstName'];
+    }
+    return (
+        <div className = "header">
+            {/* Logo */}
+            <Link className = "nav-title" to="/">
+                <img className = "nav-logo" src={ "/logo.png" } alt="React logo" />
+            </Link>
 
+            {/* Page Links */}
+            <div className = "nav-items">
+                <Link className = "nav-link" to='/Home'>Home</Link>
+                <Link className = "nav-link" to='/Restaurant'>Restaurant</Link>
+                <Link className = "nav-link" to={path}>{name}</Link>
+            </div>
+        </div>
+    )
 };
 
 export default NavBar;
