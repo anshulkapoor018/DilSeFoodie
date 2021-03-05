607bc83ab7d53f32c1d5ee7deafdc8978af4dea3
Anshul Kapoor <anshulkapoor018@gmail.com>
2021-03-05 16:01:11 -0500

    rebased from master for integration

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -25,7 +25,7 @@ const NavBar = () => {
             {/* Page Links */}
             <div className = "nav-items">
                 <Link className = "nav-link" to='/Home'>Home</Link>
-                <Link className = "nav-link" to='/Restaurant'>Restaurant</Link>
+                <Link className = "nav-link" to='/restaurants'>Restaurant</Link>
                 <Link className = "nav-link" to={path}>{name}</Link>
             </div>
         </div>
