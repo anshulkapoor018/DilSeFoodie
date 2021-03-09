bf87369a5da9acbbd8a94d578f6bcc5c9bf24e61
Anshul Kapoor <anshulkapoor018@gmail.com>
2021-03-08 22:44:53 -0500

    Redesigned navbar + search init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Redirect} from "react-router-dom";
 import Footer from "./Static/Footer"
 import Navbar from "./components/navbar.component"
@@ -10,7 +10,7 @@ import Profile from "./views/Profile/Profile";
 import Restaurants from "./components/restaurant-all.component";
 import RestaurantsPage from "./components/restaurant-single.component";
 
-
+import './App.css'
 // import NotFound from "./views/NotFound";
 
 export function onEnter(nextState, transition, callback) {
@@ -25,10 +25,7 @@ export function onEnter(nextState, transition, callback) {
 function App() {
   return (
     <Router>
-      {/* <div className="container"> */}
-        <Navbar />
-        
-        <br/>
+        <Navbar></Navbar>
         <Route exact path="/Home" component={Home} />
         <Route path="/user" component={AuthCredentials} />
         <Route path="/restaurants" component={Restaurants} />
@@ -37,9 +34,6 @@ function App() {
         <Route exact path="/">
             <Redirect to="/Home"/>
         </Route>
-        {/* <Footer/> */}
-        {/* <Route component={NotFound}/> */}
-      {/* </div> */}
     </Router>
   );
 }
