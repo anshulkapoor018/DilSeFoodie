8031dc7fee81eaa4d134d346e9619c9ec1d39748
Anshul Kapoor <anshulkapoor018@gmail.com>
2021-03-10 14:59:50 -0500

    rebase main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router,Switch, Route, Redirect} from "react-router-dom";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Redirect} from "react-router-dom";
 import Footer from "./Static/Footer"
 import Navbar from "./components/navbar.component"
 import AuthCredentials from "./components/create-user.component";
 import Home from "./views/Home/Home";
-import Header from "./views/Header/Header";
+// import Header from "./views/Header/Header";
 import Profile from "./views/Profile/Profile";
+import Restaurants from "./components/restaurant-all.component";
+import RestaurantsPage from "./components/restaurant-single.component";
+import RestaurantSearch from './components/restaurant-search.component';
+
+
+import './App.css'
 // import NotFound from "./views/NotFound";
 
 export function onEnter(nextState, transition, callback) {
@@ -20,22 +26,17 @@ export function onEnter(nextState, transition, callback) {
 
 function App() {
   return (
-    
     <Router>
-      <Navbar />
-      <div className="container">
-        
-        
-        <br/>
+        <Navbar></Navbar>
         <Route exact path="/Home" component={Home} />
         <Route path="/user" component={AuthCredentials} />
+        <Route path="/restaurants" component={Restaurants} />
+        <Route path='/restaurant/:id' component={RestaurantsPage} />
+        <Route path='/search' component={RestaurantSearch} />
         <Route exact path="/profile" component={Profile} onEnter={onEnter} />
         <Route exact path="/">
             <Redirect to="/Home"/>
         </Route>
-        <Footer/>
-        {/* <Route component={NotFound}/> */}
-      </div>
     </Router>
   );
 }
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,48 +1,28 @@
 import React from 'react';
 import './Home.css';
 
-import { Row, Col } from 'reactstrap';
-import banners from './banners.json';
-import CarouselSlider from '../../Static/CarouselSlider/index';
-import { responsiveOneItemCarousel } from '../../Static/CarouselSlider/helpers';
+// import { Row, Col } from 'reactstrap';
+// import banners from './banners.json';
+// import CarouselSlider from '../../Static/CarouselSlider/index';
+// import { responsiveOneItemCarousel } from '../../Static/CarouselSlider/helpers';
 
 // Import application sass styles
-import '../../styles/style.scss';
+// import '../../styles/style.scss';
 
 class Home extends React.PureComponent {
   render() {
       return (
         <div className='homepage'>
-          <Row className='flex-row'>
-            <Col xs='12' lg='6' className='order-lg-2 mb-3 px-3 px-md-2'>
-              <div className='home-carousel'>
-                <CarouselSlider
-                  swipeable={true}
-                  showDots={true}
-                  infinite={true}
-                  autoPlay={false}
-                  slides={banners}
-                  responsive={responsiveOneItemCarousel}
-                >
-                  {banners.map((item, index) => (
-                    <img key={index} src={item.imageUrl} alt=""/>
-                  ))}
-                </CarouselSlider>
-              </div>
-            </Col>
-            <Col xs='12' lg='3' className='order-lg-1 mb-3 px-3 px-md-2'>
-              <div className='d-flex flex-column h-100 justify-content-between'>
-                <img src='/banner-2.jpg' className='mb-3' alt=""/>
-                <img src='/banner-5.jpg' alt=""/>
-              </div>
-            </Col>
-            <Col xs='12' lg='3' className='order-lg-3 mb-3 px-3 px-md-2'>
-              <div className='d-flex flex-column h-100 justify-content-between'>
-                <img src='/banner-2.jpg' className='mb-3' alt=""/>
-                <img src='/banner-6.jpg' alt=""/>
-              </div>
-            </Col>
-          </Row>
+          <div className="search-card-center">
+            <h1>Welcome Foodies!</h1>
+            <p>Discover the best food and drinks in the Mile Square City</p>
+            <label>
+                <form id="search-form" method="POST" action="/restaurants/search">
+                    <input id="search-bar" type="text" name="search" className = "searchFields" placeholder="Search for a restaurant or cuisine" />
+                    <input type="submit" class = "btn" value="Search" />
+                </form>
+            </label> 
+          </div>
         </div>
       );
   }
