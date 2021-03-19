a2bdda5858a47a40ba1a9d999e164f1038c66ddf
Anshul Kapoor <anshulkapoor018@gmail.com>
2021-03-19 00:38:32 -0400

    reviews init

--- src/components/restaurant-single.component.js
+++ src/components/restaurant-single.component.js
@@ -3,15 +3,21 @@ import axios from 'axios';
 import './restaurant-single.component.css';
 import MapSection from '../Static/GoogleMaps';
 
+var dummyProfilePics = "https://res.cloudinary.com/helpinghands101/image/upload/v1615598217/user_mcyfxd.png"
+
 export default class RestaurantsPage extends React.Component {
   constructor(props) {
     super(props);
     this.resID = ((window.location.pathname).split("/"))[2]
-    this.state = {restaurantDetails: {}};
+    this.state = {
+      restaurantDetails: {},
+      reviewsMap: []
+    };
   }
 
   componentDidMount() {
     this.restauranListApiCall();
+    this.reviewListApiCall();
   }
 
   openInNewTab = (url) => {
@@ -24,6 +30,14 @@ export default class RestaurantsPage extends React.Component {
     axios.get('http://localhost:5000/restaurant/'+self.resID)
     .then(function (response) {
       self.setState({ restaurantDetails: response.data });
+    })
+  }
+
+  reviewListApiCall() {
+    var self = this;
+    axios.get('http://localhost:5000/review/restaurant/'+self.resID)
+    .then(function (response) {
+      self.setState({ reviewsMap: response.data });
     })
   }
 
@@ -49,6 +63,24 @@ export default class RestaurantsPage extends React.Component {
         </div>
         <div className="card-wide" id="right">
           <h2>Reviews</h2>
+          {this.state.reviewsMap.map((item, index) => (
+            <div key = {index} className = "reviews">
+              <div className = "card-full">
+                <div className="reviewHeading">
+                  <div className="reviewerName">
+                    <div className="reviewerIcon">
+                      <img src={dummyProfilePics} alt="Profile" className="profileIcon"/>
+                    </div>
+                    <h3 className = "p_inline">User Name</h3>
+                  </div>
+                  <div className="reviewerRating">
+                    <h3 className = "rating">Rating: {item.rating}</h3>
+                  </div>
+                </div>
+                <p>{item.reviewText}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     )
