599c9bbc351938372b7f52ae7bf93e71db61a6cb
NAruneshwar <57510031+NAruneshwar@users.noreply.github.com>
2021-03-21 16:25:18 -0400

    additional script to run backend

--- backend/server.js
+++ backend/server.js
@@ -47,11 +47,11 @@ app.use('/review', reviewsRouter);
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, '../../client/build')));
+  app.use(express.static(path.join(__dirname, '../../build')));
 
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+      res.sendFile(path.join(__dirname, '../../build', 'index.html'));
   });
 }
 
