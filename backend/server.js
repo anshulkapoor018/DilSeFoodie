28bd1a06024d1c70985c469aca16a02d9d9a8269
Anshul Kapoor <anshulkapoor018@gmail.com>
2021-03-22 19:08:06 -0400

    build command changed

--- backend/server.js
+++ backend/server.js
@@ -59,10 +59,10 @@ app.use('/review', reviewsRouter);
 // app.get('*', (req, res) => {
 //     res.sendFile(path.join(__dirname, '../build'))
 // })
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.resolve(__dirname, '../build')));
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../build/index.html'));
 });
 
 app.listen(port, () => {
