cc761b4cdc289952dc81973b35f48b2cd382e047
Anshul Kapoor <anshulkapoor018@gmail.com>
2021-03-22 20:36:51 -0400

    tweaks

--- backend/server.js
+++ backend/server.js
@@ -44,11 +44,13 @@ app.use('/order', ordersRouter);
 app.use('/restaurant', restaurentsRouter);
 app.use('/review', reviewsRouter);
 
-app.use(express.static(path.resolve(__dirname, '../build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, '../build')));
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.resolve(__dirname, '../build/index.html'));
-});
+  app.get('*', function(req, res) {
+    res.sendFile(path.resolve(__dirname, '../build/index.html'));
+  });
+}
 
 app.listen(port, () => {
     console.log(`${chalk.green(`Server is running on port: `)} ${chalk.blue((port))}`);
