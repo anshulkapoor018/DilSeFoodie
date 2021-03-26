7f1f3344c31af0603a1573c341d81a18002b96f8
Ikenna Ibezim <45284274+iamikenna@users.noreply.github.com>
2021-03-26 04:12:54 -0400

    Fix for preventing unauthorized user

--- client/src/App.js
+++ client/src/App.js
@@ -11,30 +11,44 @@ import 'react-notifications/lib/notifications.css';
 import {NotificationContainer} from 'react-notifications';
 import './App.css';
 
-export function onEnter(nextState, transition, callback) {
-  const { pathname } = nextState.location
-  const isLoggedIn = sessionStorage.getItem('loggedIn') === 'true'
-  if (pathname === '/user' && isLoggedIn) {
-    window.location = "/profile" //redirect to Home component
-  }
-  return callback() // go as it is.
-}
 
+//Not sure of this implementation
 
-function App() {
+// export function onEnter(nextState, transition, callback) {
+//   const { pathname } = nextState.location
+//   const isLoggedIn = sessionStorage.getItem('loggedIn') === 'true'
+//   if (pathname === '/user' && isLoggedIn) {
+//     window.location = "/profile" //redirect to Home component
+//   }
+//   return callback() // go as it is.
+// }
+
+// This was placed after line 38
+//  {/* <Route exact path="/profile" component={Profile} onEnter={onEnter} /> */}
+
+function App() { 
   return (
     <Router>
         <Navbar/>
         <NotificationContainer/>
           <Route exact path="/Home" component={Home} />
-          <Route path="/user" component={AuthCredentials} />
+          <Route exact path="/user" component={AuthCredentials} />
           <Route path="/restaurants" component={RestaurantsAll} />
           <Route path="/res/:id" component={RestaurantsPage} />
           <Route path="/search" component={RestaurantSearch} />
-          <Route exact path="/profile" component={Profile} onEnter={onEnter} />
+          <Route exact path="/profile" component={Profile} />
           <Route exact path="/">
               <Redirect to="/Home"/>
           </Route>
+          <Route exact path="/contact">
+              <Redirect to="/profile"/>
+          </Route>
+          <Route exact path="/order-history">
+              <Redirect to="/profile"/>
+          </Route>
+          
+          
+         
     </Router>
   );
 }
--- client/src/views/Profile/Profile.js
+++ client/src/views/Profile/Profile.js
@@ -9,20 +9,21 @@ import Team from '../../pages/Team';
 import React from 'react';
 
 function Profile() {
-  if (window.sessionStorage.getItem('isLoggedIn') === 'true'){
+  if (window.sessionStorage.getItem('isLoggedIn') === 'true' ){
     return (
       <Router>
-        <Sidebar />
-        <Switch>
-          <Route path='/profile' exact component={Profile2} />
-          <Route path="/contact" exact component={ContactUs} />
+       
+        {/* <Switch> */}
+          <Route exact path='/profile' component={Profile2} />
+          <Route exact path="/contact" component={ContactUs} />
           <Route path='/reports' exact component={Reports} />
-          <Route path='/order-history' exact component={UserOrderHistory} />
+          <Route path='/order-history' component={UserOrderHistory} />
           <Route path='/reports/reports1' exact component={ReportsOne} />
           <Route path='/reports/reports2' exact component={ReportsTwo} />
           <Route path='/reports/reports3' exact component={ReportsThree} />
           <Route path='/team' exact component={Team} />
-        </Switch>
+          <Sidebar />
+        {/* </Switch> */}
       </Router>
     );
   }
