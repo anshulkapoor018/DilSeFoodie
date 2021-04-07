da06f6242c3899b63385672e4c53c4ed1b102181
Ikenna Ibezim <45284274+iamikenna@users.noreply.github.com>
2021-04-07 10:09:33 -0400

    creating the accessibility component

--- client/src/components/sidebarData.component.js
+++ client/src/components/sidebarData.component.js
@@ -11,19 +11,6 @@ export const SidebarData = [
     icon: <AiIcons.AiFillHome />,
     iconClosed: <RiIcons.RiArrowDownSFill />,
     iconOpened: <RiIcons.RiArrowUpSFill />,
-
-    // subNav: [
-    //   {
-    //     title: 'Users',
-    //     path: '/overview/users',
-    //     icon: <IoIcons.IoIosPaper />
-    //   },
-    //   {
-    //     title: 'Revenue',
-    //     path: '/overview/revenue',
-    //     icon: <IoIcons.IoIosPaper />
-    //   }
-    // ]
   },
   {
     title: 'Support',
@@ -34,30 +21,10 @@ export const SidebarData = [
   },
   {
     title: 'Accessibility',
-    path: '/reports',
+    path: '/settings',
     icon: <IoIcons.IoIosPaper />,
     iconClosed: <RiIcons.RiArrowDownSFill />,
     iconOpened: <RiIcons.RiArrowUpSFill />,
-
-    // subNav: [
-    //   {
-    //     title: 'Reports',
-    //     path: '/reports/reports1',
-    //     icon: <IoIcons.IoIosPaper />,
-    //     cName: 'sub-nav'
-    //   },
-    //   {
-    //     title: 'Reports 2',
-    //     path: '/reports/reports2',
-    //     icon: <IoIcons.IoIosPaper />,
-    //     cName: 'sub-nav'
-    //   },
-    //   {
-    //     title: 'Reports 3',
-    //     path: '/reports/reports3',
-    //     icon: <IoIcons.IoIosPaper />
-    //   }
-    // ]
   },
   {
     title: 'Orders',
@@ -76,19 +43,6 @@ export const SidebarData = [
 
     iconClosed: <RiIcons.RiArrowDownSFill />,
     iconOpened: <RiIcons.RiArrowUpSFill />,
-
-    // subNav: [
-    //   {
-    //     title: 'Message 1',
-    //     path: '/messages/message1',
-    //     icon: <IoIcons.IoIosPaper />
-    //   },
-    //   {
-    //     title: 'Message 2',
-    //     path: '/messages/message2',
-    //     icon: <IoIcons.IoIosPaper />
-    //   }
-    // ]
   },
   
 ];
